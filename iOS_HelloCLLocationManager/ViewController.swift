1d989be39f034997e48e57b4d9feab8328bbfe92
Richy <richy@webcomm.com.tw>
2023-07-20 20:52:02 +0800

    import MapKit, import CoreLocation, CLLocationManager

--- iOS_HelloCLLocationManager/ViewController.swift
+++ iOS_HelloCLLocationManager/ViewController.swift
@@ -6,12 +6,29 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation // 目前位置
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var map: MKMapView!
+    var locationManager: CLLocationManager?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        locationManager = CLLocationManager()
+        locationManager?.requestWhenInUseAuthorization() // 請求授權
+        
+        if let coordinate = locationManager?.location?.coordinate{ // 目前座標
+            let xScale:CLLocationDegrees = 0.01
+            let yScale:CLLocationDegrees = 0.01
+            let span:MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: yScale, longitudeDelta: xScale)
+            
+            let region = MKCoordinateRegion(center: coordinate, span: span)
+            map.setRegion(region, animated: true)
+        }
     }
 
 
